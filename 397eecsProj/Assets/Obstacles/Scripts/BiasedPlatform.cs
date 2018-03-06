3474adb879f0c18681b8eb9e54bd15f7a3d8c84a
TakoyakiTsunami <darrenliu2019@u.northwestern.edu>
2018-03-05 18:16:44 -0600

    Added Biased Platform
    Read Title

---
+++ 397eecsProj/Assets/Obstacles/Scripts/BiasedPlatform.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+namespace Assets.Obstacles.Scripts
+{
+    public class BiasedPlatform : MonoBehaviour
+    {
+
+        public bool PlayerOne;
+        public bool PlayerTwo;
+        public GameObject Player;
+
+        // Use this for initialization
+        void Start () {
+            if (!PlayerOne && !PlayerTwo)
+            {
+                PlayerOne = true;
+            }
+        }
+	
+        // Update is called once per frame
+        void Update () {
+            if ((Player.GetComponent<Character>().MovingPlayer == 1 && PlayerOne) || (Player.GetComponent<Character>().MovingPlayer == 2 && PlayerTwo))
+            {
+                gameObject.GetComponent<BoxCollider>().enabled = true;
+            }
+            else
+            {
+                gameObject.GetComponent<BoxCollider>().enabled = false;
+            }
+        }
+    }
+}
