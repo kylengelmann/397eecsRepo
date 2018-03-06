f66d3241cb27a986a157917447d099744b5c88e7
ljhan <alyssaliu2018@u.northwestern.edu>
2018-03-05 18:13:30 -0600

    cake prefab

---
+++ 397eecsProj/Assets/UI/Scripts/CakeUI.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CakeUI : MonoBehaviour {
+
+    public Sprite[] cakeCollection;
+
+	// Use this for initialization
+	void Start () {
+        cakeCollection = new Sprite[6];
+        //for (int i = 0; i < 6; i++) {
+        //    cakeCollection[i].SpriteRenderer.enabled = false;
+        //    return;
+        //}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        for (int i = 0; i < 6; i++) {
+            if (Global.gameManager.gotCake[i]) {
+                // get corresponding icon and display it
+            }
+        }
+	}
+}
