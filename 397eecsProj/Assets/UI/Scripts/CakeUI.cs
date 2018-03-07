9ef4f7da3c992a641e922142289de7bbe832d63f
ljhan <alyssaliu2018@u.northwestern.edu>
2018-03-07 16:58:54 -0600

    added grayed out cake UI

--- 397eecsProj/Assets/UI/Scripts/CakeUI.cs
+++ 397eecsProj/Assets/UI/Scripts/CakeUI.cs
@@ -1,16 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CakeUI : MonoBehaviour {
 
     public GameObject cakes;
+    public Sprite grayCake;
+    public Sprite gotCake;
 
 	// Use this for initialization
 	void Start () {
         for (int i = 0; i < 6; i++) {
             Transform cakeT = cakes.transform.GetChild(i);
-            cakeT.gameObject.SetActive(false);
+            //cakeT.gameObject.SetActive(false);
+            Image cakeImg = cakeT.gameObject.GetComponent<Image>();
+            cakeImg.sprite = grayCake;
         }
 	}
 	
@@ -21,7 +26,9 @@ public class CakeUI : MonoBehaviour {
             if (Global.gameManager.gotCake[i])
             {
                 Transform gotCakeT = cakes.transform.GetChild(i);
-                gotCakeT.gameObject.SetActive(true);
+                //gotCakeT.gameObject.SetActive(true);
+                Image cakeImg = gotCakeT.gameObject.GetComponent<Image>();
+                cakeImg.sprite = gotCake;
             }
         }
 	}
